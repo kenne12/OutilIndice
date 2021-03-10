025d5b24eac769180abce7368e5fc5a0928624fd
Kenne Gervais <kenne_gervais@yahoo.fr>
2021-03-10 12:32:32 +0100

    Mise à jour des CNI Heure perdues : Integration des effectifs

--- OutilIndice-war/src/java/controllers/cn_incitation_heure_perdue/AbstratCritereNIHPerdueCtrl.java
+++ OutilIndice-war/src/java/controllers/cn_incitation_heure_perdue/AbstratCritereNIHPerdueCtrl.java
@@ -6,21 +6,30 @@
 package controllers.cn_incitation_heure_perdue;
 
 import entities.Categorie;
+import entities.Criterestructure;
+import entities.EffectifCategorie;
 import entities.Parametragecritere;
 import entities.Structure;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import sessions.CategorieFacadeLocal;
+import sessions.CriterestructureFacadeLocal;
+import sessions.EffectifCategorieFacadeLocal;
 import sessions.ParametragecritereFacadeLocal;
 import sessions.StructureFacadeLocal;
 import utils.Routine;
+import utils.SessionMBean;
 
 /**
  *
  * @author USER
  */
 public class AbstratCritereNIHPerdueCtrl {
+
+    @EJB
+    protected CriterestructureFacadeLocal criterestructureFacadeLocal;
+    protected Criterestructure criterestructure = new Criterestructure();
 
     @EJB
     protected ParametragecritereFacadeLocal parametragecritereFacadeLocal;
@@ -36,14 +45,23 @@ public class AbstratCritereNIHPerdueCtrl {
 
     @EJB
     protected StructureFacadeLocal structureFacadeLocal;
-    protected Structure structure = new Structure();
+    protected Structure structure = SessionMBean.getStructure();
     protected List<Structure> structures = new ArrayList<>();
+
+    @EJB
+    protected EffectifCategorieFacadeLocal effectifCategorieFacadeLocal;
+    protected List<EffectifCategorie> effectifCategories = new ArrayList<>();
 
     protected Routine routine = new Routine();
     protected String mode = "";
 
     protected double denominateurNuit = 500;
     protected double denominateurJour = 1000;
+
+    protected double totalPointMaxCritere;
+    protected double totalPointSaisiJ;
+    protected double totalPointSaisiN;
+    protected int totalEffectif;
 
     public Parametragecritere getParametragecritere() {
         return parametragecritere;
@@ -104,4 +122,21 @@ public class AbstratCritereNIHPerdueCtrl {
     public void setDenominateurJour(double denominateurJour) {
         this.denominateurJour = denominateurJour;
     }
+
+    public double getTotalPointMaxCritere() {
+        return totalPointMaxCritere;
+    }
+
+    public double getTotalPointSaisiJ() {
+        return totalPointSaisiJ;
+    }
+
+    public double getTotalPointSaisiN() {
+        return totalPointSaisiN;
+    }
+
+    public int getTotalEffectif() {
+        return totalEffectif;
+    }
+
 }
--- OutilIndice-war/src/java/controllers/cn_incitation_heure_perdue/CritereNIHPerdueCtrl.java
+++ OutilIndice-war/src/java/controllers/cn_incitation_heure_perdue/CritereNIHPerdueCtrl.java
@@ -8,6 +8,7 @@ package controllers.cn_incitation_heure_perdue;
 import controllers.util.JsfUtil;
 import entities.Categorie;
 import entities.Critere;
+import entities.EffectifCategorie;
 import entities.Parametragecritere;
 import java.io.Serializable;
 import java.util.List;
@@ -23,7 +24,7 @@ import utils.SessionMBean;
  */
 @ManagedBean
 @SessionScoped
-public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements Serializable{
+public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements Serializable {
 
     /**
      * Creates a new instance of CritereNIHPerdueCtrl
@@ -33,20 +34,31 @@ public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements
 
     @PostConstruct
     private void init() {
-        structure = SessionMBean.getStructure();
         structures.clear();
         structures.add(SessionMBean.getStructure());
         listParametres = parametragecritereFacadeLocal.findByIdStructureHp(SessionMBean.getStructure().getIdstructure(), 8, true);
         parametragecritere = new Parametragecritere();
         parametragecritere.setIdcategorie(new Categorie());
+
+        criterestructure = criterestructureFacadeLocal.findByIdStructureIdCritere(structure.getIdstructure(), 8);
+        if (criterestructure != null) {
+            totalPointMaxCritere = criterestructure.getResultat();
+        }
     }
 
-    public void prepareCreate() {
-        this.updateFiltre();
+    public void prepareCreate(String option) {
+        if (criterestructure == null) {
+            JsfUtil.addWarningMessage("Cette structure ne traite pas du bonus d'heure supplementaire");
+            return;
+        }
         mode = "Create";
         denominateurNuit = 500;
         denominateurJour = 1000;
-        RequestContext.getCurrentInstance().execute("PF('HeurePerdueCreateDialog').show()");
+        if (option.equals("1")) {
+            this.updateFiltre();
+        } else {
+            this.updateFiltre2();
+        }
     }
 
     public void prepareEdit(Parametragecritere p) {
@@ -61,19 +73,69 @@ public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements
         categories.clear();
         selectedCategories.clear();
         parametragecriteres.clear();
-        if (structure.getIdstructure() != null && structure.getIdstructure() > 0) {
-            List<Parametragecritere> list = parametragecritereFacadeLocal.findByIdStructureHp(SessionMBean.getStructure().getIdstructure(), 8, true);
-            if (list.isEmpty() || list == null) {
-                categories.addAll(categorieFacadeLocal.findAllRangeByCode());
-            } else {
-                parametragecriteres.addAll(list);
-                categories.addAll(categorieFacadeLocal.findAllRangeByCode());
-                for (Parametragecritere pc : list) {
-                    selectedCategories.add(pc.getIdcategorie());
-                }
-                categories.removeAll(selectedCategories);
-                selectedCategories.clear();
+        List<Parametragecritere> list = parametragecritereFacadeLocal.findByIdStructureHp(SessionMBean.getStructure().getIdstructure(), 8, true);
+        if (list.isEmpty() || list == null) {
+            categories.addAll(categorieFacadeLocal.findAllRangeByCode());
+        } else {
+            parametragecriteres.addAll(list);
+            categories.addAll(categorieFacadeLocal.findAllRangeByCode());
+            for (Parametragecritere pc : list) {
+                selectedCategories.add(pc.getIdcategorie());
             }
+            categories.removeAll(selectedCategories);
+            selectedCategories.clear();
+        }
+    }
+
+    public void updateFiltre2() {
+        parametragecriteres.clear();
+        List<Parametragecritere> list = parametragecritereFacadeLocal.findByIdStructureHp(SessionMBean.getStructure().getIdstructure(), 8, true);
+        if (list.isEmpty()) {
+
+            effectifCategories = effectifCategorieFacadeLocal.findByIdStructure(SessionMBean.getStructure().getIdstructure());
+            if (effectifCategories.isEmpty()) {
+                JsfUtil.addWarningMessage("Veuillez définir les effectifs par catégorie pour cette structure");
+                return;
+            }
+            for (EffectifCategorie efc : effectifCategories) {
+                Parametragecritere pc = new Parametragecritere();
+                pc.setIdparametragecritere(0l);
+                pc.setIdstructure(structure);
+                pc.setIdcritere(new Critere(8));
+                pc.setIndice(efc.getCategorie().getIndice());
+                pc.setDenominateurjournee(denominateurJour);
+                pc.setDenominateurnuit(denominateurNuit);
+                pc.setValeurjournee(pc.getIndice() / denominateurJour);
+                pc.setValeurnuit(pc.getIndice() / denominateurNuit);
+                pc.setPoint(0d);
+                pc.setIdcategorie(efc.getCategorie());
+                pc.setHeuresupp(false);
+                pc.setHeuresupn(true);
+                pc.setPratiqueprivee(false);
+                pc.setPerformanceindividuelle(false);
+                pc.setResultatqualitatifdept(false);
+                pc.setBonusrevenudept(false);
+                pc.setNombre(efc.getNombre());
+                pc.setTotal1(Math.ceil(pc.getValeurjournee() * efc.getNombre()));
+                pc.setTotal2(Math.ceil(pc.getValeurnuit() * efc.getNombre()));
+                parametragecriteres.add(pc);
+            }
+        } else {
+            parametragecriteres.addAll(list);
+        }
+        this.sommeDetail(parametragecriteres);
+        RequestContext.getCurrentInstance().execute("PF('HeurePerdueCreateDialog').show()");
+    }
+
+    private void sommeDetail(List<Parametragecritere> list) {
+        this.totalPointSaisiJ = 0;
+        this.totalPointSaisiN = 0;
+        this.totalEffectif = 0;
+
+        for (Parametragecritere pc : list) {
+            totalEffectif += pc.getNombre();
+            totalPointSaisiJ += pc.getTotal1();
+            totalPointSaisiN += pc.getTotal2();
         }
     }
 
@@ -123,20 +185,31 @@ public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements
 
     public void updateData(String mode) {
         int i = 0;
+        this.totalEffectif = 0;
+        this.totalPointSaisiJ = 0;
+        this.totalPointSaisiN = 0;
         for (Parametragecritere pc : parametragecriteres) {
             if (mode.equals("jour")) {
                 pc.setDenominateurjournee(denominateurJour);
                 pc.setValeurjournee(pc.getIndice() / denominateurJour);
+                pc.setTotal1(Math.ceil(pc.getValeurjournee() * pc.getNombre()));
                 parametragecriteres.set(i, pc);
             } else if (mode.equals("nuit")) {
                 pc.setDenominateurnuit(denominateurNuit);
                 pc.setValeurnuit(pc.getIndice() / denominateurNuit);
+                pc.setTotal2(Math.ceil(pc.getValeurnuit() * pc.getNombre()));
                 parametragecriteres.set(i, pc);
             } else {
                 pc.setValeurjournee(pc.getIndice() / pc.getDenominateurjournee());
                 pc.setValeurnuit(pc.getIndice() / pc.getDenominateurnuit());
+                pc.setTotal1(Math.ceil(pc.getValeurjournee() * pc.getNombre()));
+                pc.setTotal2(Math.ceil(pc.getValeurnuit() * pc.getNombre()));
                 parametragecriteres.set(i, pc);
             }
+
+            this.totalEffectif += pc.getNombre();
+            totalPointSaisiJ += pc.getTotal1();
+            totalPointSaisiN += pc.getTotal2();
             i++;
         }
     }
@@ -145,10 +218,14 @@ public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements
         if (mode.equals("indice")) {
             parametragecritere.setValeurjournee(parametragecritere.getIndice() / parametragecritere.getDenominateurjournee());
             parametragecritere.setValeurnuit(parametragecritere.getIndice() / parametragecritere.getDenominateurnuit());
+            parametragecritere.setTotal1(Math.ceil(parametragecritere.getValeurjournee() * parametragecritere.getNombre()));
+            parametragecritere.setTotal2(Math.ceil(parametragecritere.getValeurnuit() * parametragecritere.getNombre()));
         } else if (mode.equals("jour")) {
             parametragecritere.setValeurjournee(parametragecritere.getIndice() / parametragecritere.getDenominateurjournee());
+            parametragecritere.setTotal1(Math.ceil(parametragecritere.getValeurjournee() * parametragecritere.getNombre()));
         } else if (mode.equals("nuit")) {
             parametragecritere.setValeurnuit(parametragecritere.getIndice() / parametragecritere.getDenominateurnuit());
+            parametragecritere.setTotal2(Math.ceil(parametragecritere.getValeurnuit() * parametragecritere.getNombre()));
         }
     }
 
@@ -156,6 +233,11 @@ public class CritereNIHPerdueCtrl extends AbstratCritereNIHPerdueCtrl implements
         try {
             if (parametragecriteres.isEmpty()) {
                 JsfUtil.addErrorMessage(routine.localizeMessage("common.tableau_vide"));
+                return;
+            }
+
+            if ((totalPointSaisiJ + totalPointSaisiN) > totalPointMaxCritere) {
+                JsfUtil.addErrorMessage("Le total saisi depasse le total point max possible");
                 return;
             }
 
